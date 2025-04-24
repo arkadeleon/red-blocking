2f6e1cd914df37d5f2e5376fe46daaf7f02d8996
Leon Li <arkadeleon@gmail.com>
2025-04-24 21:27:30 +0800

    Updated frame data

--- MaCherie/Motion/MotionDownloader.swift
+++ MaCherie/Motion/MotionDownloader.swift
@@ -6,55 +6,50 @@
 //  Copyright © 2018 Leon & Vane. All rights reserved.
 //
 
-import Combine
+import ImageIO
 import UIKit
 
 class MotionDownloader {
     typealias Output = (motionInfo: MotionInfo, progress: Progress)
-    
+
     let characterCode: String
     let skillCode: String
-    
+
     init(characterCode: String, skillCode: String) {
         self.characterCode = characterCode
         self.skillCode = skillCode
     }
-    
-    func downloadPublisher() -> AnyPublisher<Output, Error> {
-        Future<MotionInfo, Error> { (promise) in
-            self.downloadJSON(promise)
-        }.flatMap { (motionInfo) -> AnyPublisher<Output, Error> in
-            var motionInfo = motionInfo
-            var merge = Empty<(Int, UIImage?), Error>().eraseToAnyPublisher()
-            for index in 0..<motionInfo.frames.count {
-                let future = Future { (promise) in
-                    self.downloadImage(at: index, promise)
-                }.handleEvents(receiveOutput: { value in
-                    motionInfo.frames[value.0].image = value.1
-                })
-                merge = merge.merge(with: future).eraseToAnyPublisher()
-            }
-            return merge.scan([]) { $0 + [$1] }.map { (images) -> Output in
-                let progress = Progress(totalUnitCount: Int64(motionInfo.frames.count))
-                progress.completedUnitCount = Int64(images.count)
-                return (motionInfo, progress)
-            }.eraseToAnyPublisher()
-        }.eraseToAnyPublisher()
+
+    func download() async throws -> MotionInfo {
+        var motionInfo = try await downloadJSON()
+
+        let images = try await downloadImages()
+
+        for index in 0..<motionInfo.frames.count {
+            motionInfo.frames[index].image = images[index].flatMap(UIImage.init)
+        }
+
+        return motionInfo
     }
-    
-    private func downloadJSON(_ promise: @escaping (Result<MotionInfo, Error>) -> ()) {
-        let path = "FrameData/\(characterCode)/\(skillCode)/\(characterCode)_\(skillCode).json"
+
+    private func downloadJSON() async throws -> MotionInfo {
+        let path = "FrameData/\(characterCode)/\(characterCode)_\(skillCode).json"
         let url = Bundle.main.resourceURL!.appendingPathComponent(path)
-        let data = try! Data(contentsOf: url)
-        let motionInfo = try! MotionInfo(data: data)
-        promise(.success(motionInfo))
+        let data = try Data(contentsOf: url)
+        let motionInfo = try MotionInfo(data: data)
+        return motionInfo
     }
-    
-    private func downloadImage(at index: Int, _ promise: @escaping (Result<(Int, UIImage?), Error>) -> ()) {
-        let path = String(format: "FrameData/%@/%@/%@_%@_%03d.png", characterCode, skillCode, characterCode, skillCode, index)
+
+    private func downloadImages() async throws -> [CGImage?] {
+        let path = String(format: "FrameData/%@/%@_%@.png", characterCode, characterCode, skillCode)
         let url = Bundle.main.resourceURL!.appendingPathComponent(path)
-        let data = try! Data(contentsOf: url)
-        let image = UIImage(data: data)
-        promise(.success((index, image)))
+        let imageSource = CGImageSourceCreateWithURL(url as CFURL, nil)!
+        let count = CGImageSourceGetCount(imageSource)
+        var images: [CGImage?] = []
+        for index in 0..<count {
+            let image = CGImageSourceCreateImageAtIndex(imageSource, index, nil)
+            images.append(image)
+        }
+        return images
     }
 }
--- MaCherie/Motion/MotionPlayerViewController.swift
+++ MaCherie/Motion/MotionPlayerViewController.swift
@@ -12,13 +12,13 @@ import Combine
 class MotionPlayerViewController: UIViewController {
     @IBOutlet var playerView: UIView!
     private var playerLayer: MotionPlayerLayer!
-    
+
     @IBOutlet var downloadProgressView: UIProgressView!
     @IBOutlet var currentFrameLabel: UILabel!
     @IBOutlet var totalFrameLabel: UILabel!
     @IBOutlet var fpsTextField: UITextField!
     @IBOutlet var progressControl: UISlider!
-    
+
     @IBOutlet var player1HitboxesView: UIView!
     @IBOutlet var player1PassiveHitboxesCheckbox: UIButton!
     @IBOutlet var player1OtherVulnerabilityHitboxesCheckbox: UIButton!
@@ -26,7 +26,7 @@ class MotionPlayerViewController: UIViewController {
     @IBOutlet var player1ThrowHitboxesCheckbox: UIButton!
     @IBOutlet var player1ThrowableHitboxesCheckbox: UIButton!
     @IBOutlet var player1PushHitboxesCheckbox: UIButton!
-    
+
     @IBOutlet var player2HitboxesView: UIView!
     @IBOutlet var player2PassiveHitboxesCheckbox: UIButton!
     @IBOutlet var player2OtherVulnerabilityHitboxesCheckbox: UIButton!
@@ -34,23 +34,22 @@ class MotionPlayerViewController: UIViewController {
     @IBOutlet var player2ThrowHitboxesCheckbox: UIButton!
     @IBOutlet var player2ThrowableHitboxesCheckbox: UIButton!
     @IBOutlet var player2PushHitboxesCheckbox: UIButton!
-    
+
     var characterCode = ""
     var skillCode = ""
-    
+
     private var observer: NSObjectProtocol!
-    
-    private var downloader: MotionDownloader?
+
     private var motionInfo: MotionInfo?
-    
+
     private var player: MotionPlayer?
-    
+
     private var downloadSubscription: AnyCancellable?
     private var playSubscription: AnyCancellable?
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
+
         observer = NotificationCenter.default.addObserver(forName: UIApplication.didEnterBackgroundNotification, object: nil, queue: nil) { _ in
             var backgroundTaskIdentifier = UIBackgroundTaskIdentifier(rawValue: 0)
             backgroundTaskIdentifier = UIApplication.shared.beginBackgroundTask(expirationHandler: {
@@ -58,46 +57,46 @@ class MotionPlayerViewController: UIViewController {
             })
         }
     }
-    
+
     deinit {
         NotificationCenter.default.removeObserver(observer)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         playerLayer = MotionPlayerLayer()
         playerLayer.frame = playerView.bounds
         playerView.layer.addSublayer(playerLayer)
-        
+
         player1PassiveHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player1PassiveHitboxHiddenKey)
         player1OtherVulnerabilityHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player1OtherVulnerabilityHitboxHiddenKey)
         player1ActiveHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player1ActiveHitboxHiddenKey)
         player1ThrowHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player1ThrowHitboxHiddenKey)
         player1ThrowableHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player1ThrowableHitboxHiddenKey)
         player1PushHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player1PushHitboxHiddenKey)
-        
+
         player2PassiveHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player2PassiveHitboxHiddenKey)
         player2OtherVulnerabilityHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player2OtherVulnerabilityHitboxHiddenKey)
         player2ActiveHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player2ActiveHitboxHiddenKey)
         player2ThrowHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player2ThrowHitboxHiddenKey)
         player2ThrowableHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player2ThrowableHitboxHiddenKey)
         player2PushHitboxesCheckbox.isSelected = !UserDefaults.standard.bool(forKey: Player2PushHitboxHiddenKey)
-        
+
         player1PassiveHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredPassiveHitboxRGBColorKey), alpha:1)
         player1OtherVulnerabilityHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredOtherVulnerabilityHitboxRGBColorKey), alpha:1)
         player1ActiveHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredActiveHitboxRGBColorKey), alpha:1)
         player1ThrowHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredThrowHitboxRGBColorKey), alpha:1)
         player1ThrowableHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredThrowableHitboxRGBColorKey), alpha:1)
         player1PushHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredPushHitboxRGBColorKey), alpha:1)
-        
+
         player2PassiveHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredPassiveHitboxRGBColorKey), alpha:1)
         player2OtherVulnerabilityHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredOtherVulnerabilityHitboxRGBColorKey), alpha:1)
         player2ActiveHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredActiveHitboxRGBColorKey), alpha:1)
         player2ThrowHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredThrowHitboxRGBColorKey), alpha:1)
         player2ThrowableHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredThrowableHitboxRGBColorKey), alpha:1)
         player2PushHitboxesCheckbox.tintColor = UIColor(rgb: UserDefaults.standard.integer(forKey: PreferredPushHitboxRGBColorKey), alpha:1)
-        
+
         let checkboxes: [UIButton] = [
             player1PassiveHitboxesCheckbox,
             player1OtherVulnerabilityHitboxesCheckbox,
@@ -112,7 +111,7 @@ class MotionPlayerViewController: UIViewController {
             player2ThrowableHitboxesCheckbox,
             player2PushHitboxesCheckbox
         ]
-        
+
         for checkbox in checkboxes {
             checkbox.layer.shadowColor = checkbox.tintColor.cgColor
             checkbox.layer.shadowRadius = 5
@@ -121,7 +120,7 @@ class MotionPlayerViewController: UIViewController {
             checkbox.layer.cornerRadius = 3
             checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         }
-        
+
         for view in [player1HitboxesView, player2HitboxesView] as [UIView] {
             view.layer.borderColor = UIColor.gray.cgColor
             view.layer.borderWidth = 1
@@ -130,32 +129,32 @@ class MotionPlayerViewController: UIViewController {
 
         prepareToDownload()
     }
-    
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+
         playerLayer.frame = playerView.bounds
     }
-    
+
     // MARK: - Action
-    
+
     @IBAction func dismiss(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func progressControlDown(_ sender: Any) {
         player?.beginSeeking()
     }
-    
+
     @IBAction func progressControlUp(_ sender: Any) {
         player?.endSeeking()
     }
-    
+
     @IBAction func progressControlSlided(_ sender: Any) {
         let frame = Int(progressControl.value)
         player?.seek(to: frame)
     }
-    
+
     @IBAction func fpsChanged(_ sender: Any) {
         var currentFPS = fpsTextField.text.flatMap { Int($0) } ?? 0
         currentFPS = max(currentFPS, 0)
@@ -163,7 +162,7 @@ class MotionPlayerViewController: UIViewController {
         fpsTextField.text = String(currentFPS)
         player?.currentFPS = currentFPS
     }
-    
+
     @IBAction func playOrPause(_ sender: UIButton) {
         if player?.state == .playing {
             player?.pause()
@@ -173,129 +172,116 @@ class MotionPlayerViewController: UIViewController {
             sender.setImage(UIImage(systemName: "pause.circle"), for: .normal)
         }
     }
-    
+
     @IBAction func forwardButtonAction(_ sender: Any) {
         player?.forward()
     }
-    
+
     @IBAction func backwardButtonAction(_ sender: Any) {
         player?.backward()
     }
-    
+
     @IBAction func togglePlayer1PassiveHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player1PassiveHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer1OtherVulnerabilityHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player1OtherVulnerabilityHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer1ActiveHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player1ActiveHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer1ThrowHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player1ThrowHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer1ThrowableHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player1ThrowableHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer1PushHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player1PushHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer2PassiveHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player2PassiveHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer2OtherVulnerabilityHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player2OtherVulnerabilityHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer2ActiveHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player2ActiveHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer2ThrowHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player2ThrowHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer2ThrowableHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player2ThrowableHitboxHiddenKey)
     }
-    
+
     @IBAction func togglePlayer2PushHitboxes(_ checkbox: UIButton) {
         checkbox.isSelected = !checkbox.isSelected
         checkbox.backgroundColor = checkbox.isSelected ? checkbox.tintColor : .clear
         UserDefaults.standard.set(!checkbox.isSelected, forKey: Player2PushHitboxHiddenKey)
     }
-    
+
     // MARK: - Playback
-    
+
     private func prepareToDownload() {
         downloadProgressView.progress = 0
-        
+
         progressControl.isUserInteractionEnabled = false
-        
-        let downloader = MotionDownloader(characterCode: characterCode, skillCode: skillCode)
-        downloadSubscription = downloader.downloadPublisher().receive(on: RunLoop.main).sink(receiveCompletion: { (completion) in
-            switch completion {
-            case .finished:
-                self.downloadProgressView.isHidden = true
-                self.progressControl.isUserInteractionEnabled = true
-                self.prepareToPlay()
-            case .failure(let error):
-                let alert = UIAlertController(title: "无法连接到服务器", message: error.localizedDescription, preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "取消", style: .cancel, handler: nil))
-                alert.addAction(UIAlertAction(title: "确定", style: .default, handler: { [unowned self] action in
-                    self.dismiss(action)
-                }))
-                alert.addAction(UIAlertAction(title: "重试", style: .default, handler: { [unowned self] _ in
-                    self.prepareToDownload()
-                }))
-                self.present(alert, animated: true, completion: nil)
-            }
-        }, receiveValue: { (value) in
-            self.motionInfo = value.motionInfo
-            
+
+        Task {
+            let downloader = MotionDownloader(characterCode: characterCode, skillCode: skillCode)
+            let motionInfo = try await downloader.download()
+
+            self.motionInfo = motionInfo
+
             self.currentFrameLabel.text = "000"
-            self.totalFrameLabel.text = String(format: "%03d", value.motionInfo.frames.count)
-            self.progressControl.maximumValue = Float(value.motionInfo.frames.count - 1)
+            self.totalFrameLabel.text = String(format: "%03d", motionInfo.frames.count)
+            self.progressControl.maximumValue = Float(motionInfo.frames.count - 1)
             self.downloadProgressView.isHidden = false
-            self.downloadProgressView.setProgress(Float(value.progress.fractionCompleted), animated: true)
-        })
-        self.downloader = downloader
+
+            self.downloadProgressView.isHidden = true
+            self.progressControl.isUserInteractionEnabled = true
+            self.prepareToPlay()
+        }
     }
-    
+
     private func prepareToPlay() {
         guard let motionInfo = motionInfo else {
             return
         }
-        
+
         let player = MotionPlayer(motionInfo: motionInfo)
         playSubscription = player.objectWillChange.sink(receiveValue: { _ in
             let currentFrame = player.currentFrame
